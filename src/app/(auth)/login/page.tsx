3a542fb415485a1fbda90b060b411393ff0bd77b
Cobancho <tomycastelli@gmail.com>
2023-11-19 15:43:58 -0300

    session page logic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,14 @@
+import { signOut } from "next-auth/react";
+import { Button } from "~/app/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
 import AuthForm from "../../components/AuthForm";
 
 const login = async () => {
   const session = await getServerAuthSession();
+
+  const logout = async () => {
+    await signOut();
+  };
 
   return (
     <div className="text-dark border-dark m-auto flex flex-col items-center space-y-4 rounded-3xl border-2 bg-white p-8 text-lg">
@@ -13,7 +19,14 @@ const login = async () => {
         Si es tu primer inicio de sesión, se creará una cuenta en este portal
         con el nombre de tu cuenta de Google
       </p>
-      {session && <AuthForm session={session} />}
+      {session ? (
+        <div>
+          <h2>{`Bienvenido ${session.user.name}, tu rol es ${session.user.role}`}</h2>
+          <Button onClick={logout}>Logout</Button>
+        </div>
+      ) : (
+        <AuthForm />
+      )}
     </div>
   );
 };
--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,36 +1,20 @@
 "use client";
 
-import type { Session } from "next-auth";
-import { signIn, signOut } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { Icons } from "./ui/Icons";
 import { Button } from "./ui/button";
 
-interface AuthFormProps {
-  session: Session;
-}
-
-const AuthForm = ({ session }: AuthFormProps) => {
+const AuthForm = () => {
   const login = async () => {
     await signIn("google");
   };
 
-  const logout = async () => {
-    await signOut();
-  };
-
   return (
     <div>
-      {session ? (
-        <div>
-          <h2>{`Bienvenido ${session.user.name}, tu rol es ${session.user.role}`}</h2>
-          <Button onClick={logout}>Logout</Button>
-        </div>
-      ) : (
-        <Button variant="outline" className="gap-2" onClick={login}>
-          <Icons.google className="h-6" />
-          Login
-        </Button>
-      )}
+      <Button variant="outline" className="gap-2" onClick={login}>
+        <Icons.google className="h-6" />
+        Login
+      </Button>
     </div>
   );
 };
