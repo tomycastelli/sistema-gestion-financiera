8311ec8a7d171c52e6e469a00d87a13f3637507b
Cobancho <tomycastelli@gmail.com>
2023-11-22 19:16:02 -0300

    Users feed

--- src/app/usuarios/permisos/page.tsx
+++ src/app/usuarios/permisos/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { api } from "~/trpc/server";
+import UsersFeed from "./UsersFeed";
 
 const Page = async () => {
   const permissions = await api.users.getAllPermissions.query({});
@@ -10,7 +10,7 @@ const Page = async () => {
       {permissions && (
         <div className="space-y-8">
           <div>
-            {permissions?.find(
+            {permissions.find(
               (permission) =>
                 permission.name === "ADMIN" ||
                 permission.name === "USERS_ROLES_VISUALIZE",
@@ -21,7 +21,7 @@ const Page = async () => {
             )}
           </div>
           <div>
-            {permissions?.find(
+            {permissions.find(
               (permission) =>
                 permission.name === "ADMIN" ||
                 permission.name === "USERS_PERMISSIONS_VISUALIZE",
@@ -30,28 +30,7 @@ const Page = async () => {
                 <h1 className="text-xl font-semibold tracking-tight">
                   Permisos
                 </h1>
-                <div className="grid grid-cols-5 gap-2">
-                  {users.map((user) => (
-                    <Link
-                      href={`/usuarios/permisos/${user.id}`}
-                      key={user.id}
-                      className="flex flex-col items-center justify-center space-y-1 rounded-xl border border-muted p-4 transition-all hover:scale-105"
-                    >
-                      <h1 className="text-lg font-semibold">{user.name}</h1>
-                      <p>{user.email}</p>
-                      <p className="text-muted-foreground">
-                        {
-                          // @ts-ignore
-                          user.permissions.length
-                        }{" "}
-                        {
-                          // @ts-ignore
-                          user.permissions.length > 1 ? "Permisos" : "Permiso"
-                        }
-                      </p>
-                    </Link>
-                  ))}
-                </div>
+                <UsersFeed initialUsers={users} />
               </div>
             )}
           </div>
