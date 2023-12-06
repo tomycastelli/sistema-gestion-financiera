08d67a190c51cfad1cb2b258349721878eac0870
Cobancho <tomycastelli@gmail.com>
2023-12-06 15:52:12 -0300

    Detalles visuales de las ids de ops, txs y mtos. Inicio de guia del sistema

---
+++ src/app/guia/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-3xl font-semibold">
+        Esta página esta en construcción
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
