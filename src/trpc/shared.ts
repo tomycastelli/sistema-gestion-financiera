fc8f0d7754b56afdb0a84538b0e9eb3d00ce2d06
Cobancho <tomycastelli@gmail.com>
2024-01-05 04:32:18 -0300

    changed getBaseUrl to the nextauth url

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -8,6 +8,7 @@ export const transformer = superjson;
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
