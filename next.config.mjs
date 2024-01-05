5185025d541e12ba156bd3e8d001b68aa7a84b56
Cobancho <tomycastelli@gmail.com>
2024-01-05 03:30:53 -0300

    Setting up SST deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx"],
-  output: "standalone",
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
