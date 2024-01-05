2248f15ecd6079a7320b94c47c76d9018f4116fb
Cobancho <tomycastelli@gmail.com>
2024-01-05 17:31:42 -0300

    removed sst completely

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
