3c858148833ce44feb0ea37592b00c75093c92cd
Cobancho <tomycastelli@gmail.com>
2024-01-05 13:31:18 -0300

    added a hardcoded link in case of existing a aws access key environment variable

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -8,7 +8,8 @@ export const transformer = superjson;
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
+  if (process.env.AWS_ACCESS_KEY_ID)
+    return "https://d30zt9jpiei5fi.cloudfront.net";
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
