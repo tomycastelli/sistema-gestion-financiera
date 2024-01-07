8b114cbea213d1135d41d9b3169fabf1ab004c13
Cobancho <tomycastelli@gmail.com>
2024-01-07 20:36:11 -0300

    changed env validation and added missing envs in building

--- src/env.mjs
+++ src/env.mjs
@@ -17,7 +17,7 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
+      process.env.VERCEL ? z.string() : z.string(),
     ),
     // Add ` on ID and SECRET if you want to make sure they're not empty
     GOOGLE_CLIENT_ID: z.string(),
