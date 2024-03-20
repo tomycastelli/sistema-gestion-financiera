9546f85e838b1ca84468709cd382f705a51287ce
Cobancho <tomycastelli@gmail.com>
2024-03-20 19:06:59 -0300

    Changed config to remove million/liint

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
-import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx"],
   output: "standalone",
   images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "du502cbk6jn66.cloudfront.net",
-      port: "",
-      pathname: "/content/images/**"
-    }]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "du502cbk6jn66.cloudfront.net",
+        port: "",
+        pathname: "/content/images/**",
+      },
+    ],
+  },
 };
-export default MillionLint.next({
-  rsc: true
-})(nextConfig);
+export default nextConfig;
+
