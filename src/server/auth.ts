6895cafbdd35962eb3882415e4a5fa0bc09557b3
Cobancho <tomycastelli@gmail.com>
2024-03-21 00:03:03 -0300

    Changed to using standard vercel url instead of deployment one

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -14,7 +14,7 @@ const adapter = new DrizzlePostgreSQLAdapter(db, session, user);
 
 const baseUrl =
   env.NODE_ENV === "production"
-    ? env.VERCEL_URL ? `https://${env.VERCEL_URL}` : "https://sistema.maika.com.ar"
+    ? env.VERCEL_URL ? `https://sistema-maika.vercel.app` : "https://sistema.maika.com.ar"
     : "http://localhost:3000";
 
 const redirectUrl = `${baseUrl}/api/auth/callback/azure-ad`;
