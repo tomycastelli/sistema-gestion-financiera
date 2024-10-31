0c41ad627a71389c8da4917aeb716442e8685396
Cobancho <tomycastelli@gmail.com>
2024-10-31 20:35:12 -0300

    middleware automatico

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const authCookie = request.cookies.get("auth_session");
+
+  // Exclude the root URL and other public URLs from the middleware check
+  if (
+    request.nextUrl.pathname === "/" ||
+    request.nextUrl.pathname.startsWith("/public")
+  ) {
+    return NextResponse.next();
+  }
+
+  if (!authCookie?.value) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/cuentas/:path",
+    "/entidades/:path",
+    "/operaciones/:path",
+    "/preferencias/:path",
+  ],
+};
